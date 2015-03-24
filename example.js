3bece51fa35f85330f00000ab75eb36274b99b34
Dylan Fox <dylan@crowdvance.com>
2015-03-24 19:02:04 -0400

    First pages commit for example

---
+++ example.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	$("#examples h1").decrypt_effect({
+		decrypted_text: "decrypt_effect.js examples!",
+	});
+
+	$("#examples #slow-example button").on("click", function(){
+		$("#slow-example h3").decrypt_effect({
+			speed: 100,
+			decrypted_text: "sloooooooow example"
+		});
+	});
+
+	$("#examples #fast-example button").on("click", function(){
+		$("#fast-example h3").decrypt_effect({
+			speed: 10,
+			decrypted_text: "fast example!"
+		});
+	});
+
+	$("#examples #custom-example button").on("click", function(){
+		var speed = $("input#speed").val();
+		$("#custom-example h3").decrypt_effect({
+			speed: speed,
+			decrypted_text: "custom speed example"
+		});
+	});		
+
+});
